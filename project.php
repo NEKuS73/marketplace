ac1ff0ba32e92121e87bcbbb62a04b94157d2d45
NEKuS <nekitkuz1004@mail.ru>
2026-05-24 22:49:22 +0300

    Marketplace scaffold

---
+++ project.php
@@ -0,0 +1,10 @@
+<?php
+$products = [
+    ['id' => 1, 'name' => 'Product A', 'price' => 100],
+    ['id' => 2, 'name' => 'Product B', 'price' => 250],
+    ['id' => 3, 'name' => 'Product C', 'price' => 350],
+];
+
+$users = [];
+$orders = [];
+
